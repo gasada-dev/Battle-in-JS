730dde33c1747550a75a212f2cd2023fba3ffbb9
gasada <gasada.dev@gmail.com>
2024-02-03 01:21:49 +0300

    feat: new design

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ import {Player} from './src/js/classes/Player.js';
 
 let enemies = new GenerateEnemy(Character).generate(10);
 
-let man = new Player('man', 10, 100, 5, 2);
+let hero = new Player('hero', 10, 100, 5, 2);
 
-let game = new Game(man, enemies);
+let game = new Game(hero, enemies);
 game.start();
 
 
--- src/js/Classes/Game.js
+++ src/js/Classes/Game.js
@@ -7,15 +7,44 @@ export class Game {
     constructor(player, enemies) {
         this.player = player;
         this.enemies = enemies;
+        this.isRunning = "running";
     }
 
-    
+
     start() {
         this.player.attack(this.enemies[0]);
+
+        while (this.isRunning === "running") {
+
+
+
+        };
+    }
+        
+    battle() {
+
+        
         this.player.attack(this.enemies[0]);
-        this.player.attack(this.enemies[0]);
-        this.player.attack(this.enemies[0]);
-        this.player.attack(this.enemies[0]);
-        this.player.attack(this.enemies[0]);
+        
+        this.enemies[0].attack(this.player);
+        
+    }
+
+        // switch (this.isRunning) {
+        //     case "stop":
+        //         break;
+        //     case "pause":
+        //         break;
+        //     default:
+        //         break;
+        // }
+
+
+    stopGame() {
+        this.isRunning = "stop";
+    }
+
+    pauseGame() {
+        this.isRunning = "pause";
     }
 }
